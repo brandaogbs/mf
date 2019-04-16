b0d1f30aaa4c1580556584e9e100aee653ed2f2a
brandaogbs <brandaogbs@gmail.com>
2019-04-16 15:23:03 -0300

    fix backprog method

--- miniflow.py
+++ miniflow.py
@@ -1,15 +1,24 @@
 import numpy as np
 
 class Node(object):
+    """
+    Base class for nodes in the network.
+    Arguments:
+        `inbound_nodes`: A list of nodes with edges into this node.
+    """
     def __init__(self, inbound_nodes=[]):
         self.inbound_nodes = inbound_nodes
-        self.outbound_nodes = []
         self.value = None
-        for n in self.inbound_nodes:
-            n.outbound_nodes.append(self)
+        self.outbound_nodes = []
+        self.gradients = {}
+        for node in inbound_nodes:
+            node.outbound_nodes.append(self)
 
     def forward(self):
-        raise NotImplemented
+        raise NotImplementedError
+
+    def backward(self):
+        raise NotImplementedError
 
 
 class Input(Node):
@@ -19,6 +28,12 @@ class Input(Node):
     def forward(self, value=None):
         if value is not None:
             self.value = value
+    def backward(self):
+        # NOTE: An Input node has no inputs so the gradient is zero.
+        self.gradients = {self: 0}
+        for n in self.outbound_nodes:
+            grad_cost = n.gradients[self]
+            self.gradients[self] += grad_cost * 1
 
 
 class Add(Node):
@@ -52,6 +67,18 @@ class Linear(Node):
 
         self.value = np.dot(X,W) + b
 
+    def backward(self):
+        """
+        Calculates the gradient based on the output values.
+        """
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_nodes}
+
+        for n in self.outbound_nodes:
+            grad_cost = n.gradients[self]
+            self.gradients[self.inbound_nodes[0]] += np.dot(grad_cost, self.inbound_nodes[1].value.T)
+            self.gradients[self.inbound_nodes[1]] += np.dot(self.inbound_nodes[0].value.T, grad_cost)
+            self.gradients[self.inbound_nodes[2]] += np.sum(grad_cost, axis=0, keepdims=False)
+
 
 class Sigmoid(Node):
     def __init__(self, node):
@@ -65,6 +92,44 @@ class Sigmoid(Node):
         input_value = self.inbound_nodes[0].value
         self.value = self._sigmoid(input_value)
 
+    def backward(self):
+        """
+        Calculates the gradient using the derivative of
+        the sigmoid function.
+        """
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_nodes}
+
+        for n in self.outbound_nodes:
+            grad_cost = n.gradients[self]
+            sigmoid = self.value
+            self.gradients[self.inbound_nodes[0]] += sigmoid * (1 - sigmoid) * grad_cost
+
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+        self.m = self.inbound_nodes[0].value.shape[0]
+
+        self.diff = y - a
+        self.value = np.mean(np.square(self.diff))
+
+    def backward(self):
+        """
+        Calculates the gradient of the cost.
+
+        This is the final node of the network so outbound nodes
+        are not a concern.
+        """
+
+        self.gradients[self.inbound_nodes[0]] = (2 / self.m) * self.diff
+        self.gradients[self.inbound_nodes[1]] = (-2 / self.m) * self.diff
 
 
 def topological_sort(feed_dict):
@@ -105,8 +170,29 @@ def topological_sort(feed_dict):
     return L
 
 
-def forward_pass(output_node, sorted_nodes):
-    for n in sorted_nodes:
+def forward_and_backward(graph):
+    """
+    Performs a forward pass and a backward pass through a list of sorted nodes.
+    Arguments:
+        `graph`: The result of calling `topological_sort`.
+    """
+    # Forward pass
+    for n in graph:
         n.forward()
 
-    return output_node.value
+    # Backward pass
+    for n in graph[::-1]:
+        n.backward()
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Nodes representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    for t in trainables:
+        p = t.gradients[t]
+        t.value = t.value - learning_rate*p
